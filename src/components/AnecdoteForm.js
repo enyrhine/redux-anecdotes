25ce156a1a986df47f42de80064fe8217591ae14
Elsa Nyrhinen <elsa.nyrhinen@helsinki.fi>
2019-03-04 16:15:41 +0200

    tehtävä 6.10

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { createNotification } from '../reducers/notificationReducer';
 
 const NewAnecdote = (props) => {
 
     const addAnecdote = (event) => {
         event.preventDefault()
         props.store.dispatch(createAnecdote(event.target.anecdote.value))
+        props.store.dispatch(createNotification(`Lisätty uusi anekdootti: ${event.target.anecdote.value}`))
+
+        setTimeout(() => {
+            props.store.dispatch(createNotification(''))
+        }, 5000)
         event.target.anecdote.value = ''
     }
 
