437f39fc720697b74687f42b8b128b4181f38f61
Elsa Nyrhinen <elsa.nyrhinen@helsinki.fi>
2019-03-04 12:52:50 +0200

    tehtävä 6.7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer';
+import { voteAnecdote } from './reducers/anecdoteReducer';
+import NewAnecdote from './components/AnecdoteForm'
 
 const App = (props) => {
   const anecdotes = props.store.getState()
@@ -8,12 +9,8 @@ const App = (props) => {
     console.log('vote', id)
     props.store.dispatch(voteAnecdote(id))
   }
+
   
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    props.store.dispatch(createAnecdote(event.target.anecdote.value))
-    event.target.anecdote.value = ''
-  }
 
   return (
     <div>
@@ -29,11 +26,7 @@ const App = (props) => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote"/></div>
-        <button type="submit">create</button>
-      </form>
+      <NewAnecdote store={props.store}/>
     </div>
   )
 }
