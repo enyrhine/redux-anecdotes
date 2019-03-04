017d197fecf4349096239416953b3a7151a6f888
Elsa Nyrhinen <elsa.nyrhinen@helsinki.fi>
2019-03-04 13:11:37 +0200

    tehtävä 6.8

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from 'react';
-import { voteAnecdote } from './reducers/anecdoteReducer';
 import NewAnecdote from './components/AnecdoteForm'
+import ListAnecdote from './components/AnecdoteList';
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    props.store.dispatch(voteAnecdote(id))
-  }
-
-  
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <NewAnecdote store={props.store}/>
+      <ListAnecdote store={props.store} />
+      <NewAnecdote store={props.store} />
     </div>
   )
 }
